604fdba16f9a0310d3b3797aaa9a1c2cfb997734
youngban <qksud14@gmail.com>
2024-07-09 08:17:00 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import OpenAI from "openai";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+async function main() {
+  console.log("connect to open ai");
+}
+
+main();
